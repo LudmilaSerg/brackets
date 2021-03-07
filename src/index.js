3090fa56e2fe36a6c4886234d60743abedea5d83
LudmilaSerg <mikoelectrik@gmail.com>
2021-03-07 18:54:02 +0300

    feat: done js task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var arr = [];
+  var existBracket = {};
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    existBracket[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  arr.push(str.charAt(0));
+
+  for (var j = 1; j < str.length; j++) {
+    if (str.charAt(j) === existBracket[arr[arr.length - 1]]) {
+      arr.pop();
+    } else {
+      arr.push(str.charAt(j));
+    }
+  }
+  if (arr.length > 0) { return false; }
+  return true;
+};
